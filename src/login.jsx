16f2387fdced4d12985f970258b7882d3c3e840b
ujwal-l <ujwal565@gmail.com>
2025-01-19 11:09:15 +0530

    Initial commit

---
+++ src/login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <div style={{ backgroundColor: '#C41230' }}>
+        <div className='min-h-screen'>
+          <h1
+            style={{
+              color: 'white',
+              fontSize: 100,
+              fontWeight: 'bolder',
+              fontFamily: 'Friz Quadrata, Arial, sans-serif',
+            }}
+          >
+            FINGER LICKIN' GOOD
+          </h1>
+        </div>
+        <div className='login-container'>
+          <h1>Login</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
